d61b52ad518d28ab517634529df769d1c3a1b398
Ruthmy <ruthmy17@gmail.com>
2023-10-10 14:41:33 -0600

    Modified 'recipes_controller.rb' -controller- - Added authenticate method with exception on public recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: [:public_recipe]
+
   def index
     @recipes = Recipe.all
   end
@@ -26,53 +28,3 @@ class RecipesController < ApplicationController
     params.require(:recipe).permit(:name, :ingredients, :cook_time)
   end
 end
-
-# ```
-#
-### Edit
-#
-# ```ruby
-# Path: app/views/recipes/edit.html.erb
-# <h1>Edit Recipe</h1>
-#
-# <%= form_for @recipe do |f| %>
-#   <%= f.label :name %>
-#   <%= f.text_field :name %><br>
-#
-#   <%= f.label :ingredients %>
-#   <%= f.text_area :ingredients %><br>
-#
-#   <%= f.label :cook_time %>
-#   <%= f.text_field :cook_time %><br>
-#
-#   <%= f.submit %>
-# <% end %>
-# ```
-#
-# ```ruby
-# Path: app/controllers/recipes_controller.rb
-# class RecipesController <p ApplicationController
-#   def edit
-#     @recipe = Recipe.find_by(id: params[:id])
-#   end
-#
-#   def update
-#     @recipe = Recipe.find_by(id: params[:id])
-#     @recipe.update(recipe_params)
-#     redirect_to recipe_path(@recipe)
-#   end
-# end
-# ```
-#
-### Delete
-#
-# ```ruby
-# Path: app/views/recipes/show.html.erb
-# <h1><%= @recipe.name %></h1>
-#
-# <p><strong>Ingredients:</strong> <%= @recipe.ingredients %></p>
-# <p><strong>Cook Time:</strong> <%= @recipe.cook_time %></p>
-#
-# <%= link_to "Edit Recipe", edit_recipe_path(@recipe) %>
-# <%= link_to "Delete Recipe", recipe_path(@recipe), method: :delete, data: { confirm: "Are you sure?" } %>
-# ```
