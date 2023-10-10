7b7700214c7504e1e51aa6378164ba2a81d71613
Ruthmy <ruthmy17@gmail.com>
2023-10-10 14:40:54 -0600

    Modified 'users_controller.rb' -controller- - Added authenticate method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   # GET /users
   def index
     @users = User.all
@@ -11,36 +13,6 @@ class UsersController < ApplicationController
     return unless @user.nil?
 
     flash[:error] = 'User not found'
-    redirect_to users_path
-  end
-
-  # GET /users/new
-  def new
-    # Create a new user object
-    @user = User.new
-  end
-
-  # POST /users
-  def create
-    # Create a new user object with the given params
-    @user = User.new(user_params)
-    # If the user object is saved to the database
-    if @user.save
-      # Redirect to the user's page
-      redirect_to @user
-    else
-      # Otherwise, render the new user form again
-      render 'new'
-    end
-  end
-
-  # DELETE /users/:id
-  def destroy
-    # Find the user object with the given id
-    @user = User.find(params[:id])
-    # Delete the user object from the database
-    @user.destroy
-    # Redirect to the users page
     redirect_to users_path
   end
 end
