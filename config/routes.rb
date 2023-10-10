12306669614b0c81bf52cc80033a0d00256b5c98
Ruthmy <ruthmy17@gmail.com>
2023-10-10 14:24:58 -0600

    Modified 'routes.rb' -routes- - Added the routes according with the possible pages.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "recipes#public_recipe"
+  resources :users, only: %i[index show new create destroy]
+  resources :recipes, only: %i[index show new create destroy]
+  get '/public_recipe', to: 'recipes#public_recipe', as: 'public_recipe'
+  resources :foods, only: %i[index show new create destroy]
 end
