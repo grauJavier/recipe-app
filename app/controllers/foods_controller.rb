fe8d3405302bd9669cbc1d83d25e720eba4831f6
Ruthmy <ruthmy17@gmail.com>
2023-10-10 14:00:47 -0600

    Created 'foods_controller.rb' -controller- - Created with the methods index, show, new, create and destroy.

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,51 @@
+class FoodsController < ApplicationController
+  # GET /foods
+  def index
+    # Get all the food objects from the database
+    @foods = Food.all
+  end
+
+  # GET /foods/:id
+  def show
+    # Find the food object with the given id
+    @food = Food.find_by_id(params[:id])
+
+    # If the food object is not found
+    return unless @food.nil?
+
+    # Set an error message
+    flash[:error] = 'Food not found'
+    # Redirect to the foods page
+    redirect_to foods_path
+  end
+
+  # GET /foods/new
+  def new
+    # Create a new food object
+    @food = Food.new
+  end
+
+  # POST /foods
+  def create
+    # Create a new food object with the given params
+    @food = Food.new(food_params)
+    # If the food object is saved to the database
+    if @food.save
+      # Redirect to the food's page
+      redirect_to @food
+    else
+      # Otherwise, render the new food form again
+      render 'new'
+    end
+  end
+
+  # DELETE /foods/:id
+  def destroy
+    # Find the food object with the given id
+    @food = Food.find(params[:id])
+    # Delete the food object from the database
+    @food.destroy
+    # Redirect to the foods page
+    redirect_to foods_path
+  end
+end
