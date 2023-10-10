75c067db03ab598ac0d31937bdff3dc2d3b5a807
Ruthmy <ruthmy17@gmail.com>
2023-10-10 14:29:05 -0600

    Created AddDevise migration - Install Devise in the project - Modified 'schema.rb' - Modified 'user.rb' -model- added devise methods - Added devise initializators

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :recipes
   has_many :foods
 
