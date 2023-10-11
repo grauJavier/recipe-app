8eb2087cc2bd790b6cc129264adc9ef872b822e5
Ruthmy <ruthmy17@gmail.com>
2023-10-11 14:42:31 -0600

    Modified 'food.rb' -model- - Change lines 21-22 to add the 'greater_or_equal_than' conditional

--- app/models/food.rb
+++ app/models/food.rb
@@ -18,7 +18,7 @@ class Food < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :measurement_unit, presence: true, length: { maximum: 15 }
-  validates :price, presence: true, numericality: { greater_than: 0 }
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_or_equal_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_or_equal_than: 0 }
   validates :user_id, presence: true
 end
