fbccbfbcdc0adbaaaff441c25d20f0500759a1d2
Ruthmy <ruthmy17@gmail.com>
2023-10-12 14:20:40 -0600

    Modified 'recipe_foods_controller.rb' -controller- - Fixed the new and create method to successfully add a new relation. - Change the recipe_food_params to just use food_id and quantity

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -4,13 +4,15 @@ class RecipeFoodsController < ApplicationController
   # GET /recipe_foods/new
   def new
     @recipe_food = RecipeFood.new
+    @recipe = Recipe.find(params[:recipe_id])
     # List all the foods that the current user has except the ones that are already in the recipe
-    @foods = current_user.foods.where.not(id: RecipeFood.where(recipe_id: params[:recipe_id]).pluck(:food_id))
+    @foods = current_user.foods.where.not(id: @recipe.recipe_foods.pluck(:food_id))
   end
 
   # POST /recipe_foods
   def create
     @recipe_food = RecipeFood.new(recipe_food_params)
+    @recipe_food.recipe_id = params[:recipe_food][:recipe_id]
 
     if @recipe_food.save
       redirect_to @recipe_food, notice: 'Ingredient was successfully added to the recipe.'
@@ -19,7 +21,9 @@ class RecipeFoodsController < ApplicationController
     end
   end
 
+  private
+
   def recipe_food_params
-    params.require(:recipe_food).permit(:recipe_id, :food_id, :quantity)
+    params.require(:recipe_food).permit(:food_id, :quantity)
   end
 end
