ba12364cced6f9a7f7c9b90429cd3bd79d0d24c4
Ruthmy <ruthmy17@gmail.com>
2023-10-12 11:16:36 -0600

    Modified 'recipe_foods_controller.rb' -controller- - Added an instance variable for the inventary

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -4,6 +4,8 @@ class RecipeFoodsController < ApplicationController
   # GET /recipe_foods/new
   def new
     @recipe_food = RecipeFood.new
+    # List all the foods that the current user has except the ones that are already in the recipe
+    @foods = current_user.foods.where.not(id: RecipeFood.where(recipe_id: params[:recipe_id]).pluck(:food_id))
   end
 
   # POST /recipe_foods
@@ -15,5 +17,9 @@ class RecipeFoodsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:recipe_id, :food_id, :quantity)
   end
 end
